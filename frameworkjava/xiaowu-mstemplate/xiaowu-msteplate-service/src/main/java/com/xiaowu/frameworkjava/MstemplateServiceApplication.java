28f904102780074e1eb0ef8b841e531bf4d1ae72
xiao-wu-z <2721059902@qq.com>
2025-09-18 22:45:43 +0800

    nacos集成

---
+++ frameworkjava/xiaowu-mstemplate/xiaowu-msteplate-service/src/main/java/com/xiaowu/frameworkjava/MstemplateServiceApplication.java
@@ -0,0 +1,11 @@
+package com.xiaowu.frameworkjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MstemplateServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MstemplateServiceApplication.class, args);
+    }
+}
