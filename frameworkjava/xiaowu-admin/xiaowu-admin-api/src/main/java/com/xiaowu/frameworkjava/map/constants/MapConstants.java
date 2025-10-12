17df0862d2866ef6f26a598ef5b21adf2fa6d187
xiao-wu-z <2721059902@qq.com>
2025-10-12 20:49:39 +0800

    最终版查询城市列表功能

--- frameworkjava/xiaowu-admin/xiaowu-admin-api/src/main/java/com/xiaowu/frameworkjava/map/constants/MapConstants.java
+++ frameworkjava/xiaowu-admin/xiaowu-admin-api/src/main/java/com/xiaowu/frameworkjava/map/constants/MapConstants.java
@@ -5,4 +5,9 @@ public class MapConstants {
      * 城市级别
      */
     public final static Integer CITY_LEVEL = 2;
+
+    /**
+     * 城市列表缓存key
+     */
+    public final static String CACHE_MAP_CITY_KEY = "map:city:id";
 }
--- frameworkjava/xiaowu-admin/xiaowu-admin-service/src/main/java/com/xiaowu/frameworkjava/map/service/impl/MapServiceImpl.java
+++ frameworkjava/xiaowu-admin/xiaowu-admin-service/src/main/java/com/xiaowu/frameworkjava/map/service/impl/MapServiceImpl.java
@@ -1,16 +1,24 @@
 package com.xiaowu.frameworkjava.map.service.impl;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.xiaowu.frameworkjava.map.constants.MapConstants;
 import com.xiaowu.frameworkjava.map.domain.dto.SysRegionDTO;
 import com.xiaowu.frameworkjava.map.domain.entity.SysRegion;
 import com.xiaowu.frameworkjava.map.mapper.RegionMapper;
 import com.xiaowu.frameworkjava.map.service.IMapService;
+import com.xiaowu.frameworkjava.service.RedisService;
+import com.xiaowu.frameworkjava.utils.CacheUtil;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.benmanes.caffeine.cache.Cache;
+
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 @Service
@@ -22,8 +30,44 @@ public class MapServiceImpl implements IMapService {
     @Autowired
     private RegionMapper regionMapper;
 
-    @Override
-    public List<SysRegionDTO> getCityList() {
+    /**
+     * redis服务
+     */
+    @Autowired
+    private RedisService redisService;
+
+    /**
+     *  caffeine缓存
+     */
+    @Autowired
+    private Cache<String, Object> caffeineCache;
+
+    @PostConstruct
+    public void initCityMap() {
+        List<SysRegion> regionList = regionMapper.selectAllRegion();
+        // 在服务加载之前缓存城市列表数据
+        loadCityInfo(regionList);
+    }
+
+    private void loadCityInfo(List<SysRegion> regionList) {
+        List<SysRegionDTO> regionDTOList = new ArrayList<>();
+
+        for (SysRegion region : regionList) {
+            if (region.getLevel().equals(MapConstants.CITY_LEVEL)) {
+                SysRegionDTO regionDTO = new SysRegionDTO();
+                BeanUtils.copyProperties(region, regionDTO);
+                regionDTOList.add(regionDTO);
+            }
+        }
+        CacheUtil.setL2Cache(redisService, MapConstants.CACHE_MAP_CITY_KEY,
+                regionDTOList, caffeineCache, 120L, TimeUnit.MINUTES);
+    }
+
+    /**
+     * 获取城市列表V1方案 -- 直接查询数据库
+     * @return 列表数据
+     */
+    public List<SysRegionDTO> getCityListV1() {
         List<SysRegionDTO> regionDTOList = new ArrayList<>();
 
         List<SysRegion> regionList = regionMapper.selectAllRegion();
@@ -35,5 +79,68 @@ public class MapServiceImpl implements IMapService {
             }
         }
         return regionDTOList;
+    }
+
+    /**
+     * 获取城市列表V2方案 -- 先查询Redis缓存，Redis缓存中没有再查询数据库
+     * @return 列表数据
+     */
+    public List<SysRegionDTO> getCityListV2() {
+        List<SysRegionDTO> regionDTOList = new ArrayList<>();
+
+        regionDTOList = redisService.getCacheObject(MapConstants.CACHE_MAP_CITY_KEY,
+                new TypeReference<List<SysRegionDTO>>() {});
+        if (regionDTOList != null) {
+            return regionDTOList;
+        }
+        List<SysRegion> regionList = regionMapper.selectAllRegion();
+        for (SysRegion region : regionList) {
+            if (region.getLevel().equals(MapConstants.CITY_LEVEL)) {
+                SysRegionDTO regionDTO = new SysRegionDTO();
+                BeanUtils.copyProperties(region, regionDTO);
+                regionDTOList.add(regionDTO);
+            }
+        }
+        redisService.setCacheObject(MapConstants.CACHE_MAP_CITY_KEY, regionDTOList);
+        return regionDTOList;
+    }
+
+    /**
+     * 获取城市列表V3方案 -- 引入二级缓存
+     * @return 列表数据
+     */
+    public List<SysRegionDTO> getCityListV3() {
+        List<SysRegionDTO> regionDTOList = new ArrayList<>();
+
+        regionDTOList = CacheUtil.getL2Cache(redisService, MapConstants.CACHE_MAP_CITY_KEY,
+                new TypeReference<List<SysRegionDTO>>() {}, caffeineCache);
+
+        if (regionDTOList != null) {
+            return regionDTOList;
+        }
+        List<SysRegion> regionList = regionMapper.selectAllRegion();
+        for (SysRegion region : regionList) {
+            if (region.getLevel().equals(MapConstants.CITY_LEVEL)) {
+                SysRegionDTO regionDTO = new SysRegionDTO();
+                BeanUtils.copyProperties(region, regionDTO);
+                regionDTOList.add(regionDTO);
+            }
+        }
+        CacheUtil.setL2Cache(redisService, MapConstants.CACHE_MAP_CITY_KEY, regionDTOList,
+                caffeineCache, 120L, TimeUnit.MINUTES);
+
+        return regionDTOList;
+    }
+
+    /**
+     * 获取城市列表V4方案 -- 缓存预热方案
+     * @return 缓存列表数据
+     */
+    @Override
+    public List<SysRegionDTO> getCityList() {
+        List<SysRegionDTO> cache = CacheUtil.getL2Cache(redisService,
+                MapConstants.CACHE_MAP_CITY_KEY,
+                new TypeReference<List<SysRegionDTO>>() {}, caffeineCache);
+        return cache;
     }
 }
